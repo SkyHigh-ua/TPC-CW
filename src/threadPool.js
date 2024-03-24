691c5c4b450a0b3387a962a624093c039cdbe281
SkyHighConstruction <rybalka.illya@gmail.com>
2024-03-24 21:23:15 +0200

    removed unused method

--- src/threadPool.js
+++ src/threadPool.js
@@ -12,27 +12,6 @@ export class ThreadPool {
     }
   }
 
-  execute(task, callback) {
-    if (this.shuttingDown) {
-        callback(new Error('ThreadPool is shutting down'));
-        return;
-    }
-    if (this.workers.length === 0) {
-      this.tasks.push({ task, callback });
-    } else {
-      const worker = this.workers.pop();
-      worker.once('message', (result) => {
-        callback(result);
-        this.workers.push(worker);
-        if (this.tasks.length > 0) {
-          const { task, callback } = this.tasks.shift();
-          this.execute(task, callback);
-        }
-      });
-      worker.postMessage(task);
-    }
-  }
-
   submit(task) {
     return new Promise((resolve, reject) => {
       if (this.shuttingDown) {
